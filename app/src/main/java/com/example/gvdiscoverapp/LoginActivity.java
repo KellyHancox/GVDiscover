f2774f8c6f978c14b666f172fa5b9848eb8391d4
Matthew Shan <matthewshan99@gmail.com>
2019-02-04 19:33:05 -0500

    Guest button added

--- app/src/main/java/com/example/gvdiscoverapp/LoginActivity.java
+++ app/src/main/java/com/example/gvdiscoverapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.gvdiscoverapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -63,7 +64,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View mLoginFormView;
 
 
-
+    public void toHub(View view) {
+        startActivity(new Intent(LoginActivity.this, createEventsMatt.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
