309136ece72b5ea3e2ba67c6b9faf96202b768ee
matthewshan <matthewshan99@gmail.com>
2019-03-17 15:46:13 -0400

    Created new classes that we designed: Model and GVUser

---
+++ app/src/main/java/com/example/gvdiscoverapp/Model.java
@@ -0,0 +1,51 @@
+package com.example.gvdiscoverapp;
+
+import java.util.HashMap;
+
+public class Model {
+    /** An instance of the Singleton model */
+    private static Model instance = null;
+    /** Holds the current user in session */
+    private static GVUser user;
+    /** Holds all the events in the database.
+     * Value format: "location~~date~~startTime~~endTime~~desc */
+    public static HashMap<String, String> events;
+
+
+    private Model() {
+        events = new HashMap<>();
+    }
+
+    public static Model getInstance() {
+        if(instance == null) {
+            instance = new Model();
+        }
+        return instance;
+    }
+
+    public static void newUser(String email) {
+        user = new GVUser(email);
+    }
+
+    public static void addEvent(String eventName, String event) {
+        events.put(eventName, event);
+    }
+
+    /***
+     *  Gets the events stored in model.
+     *  Value format: "location~~date~~startTime~~endTime~~desc
+     *
+     *  @return Hash map were the key is the name of the event.
+     * */
+    public static HashMap<String, String> getEvents() {
+        return events;
+    }
+
+    public static void SignUp(String eventName) {
+        user.addEvent(eventName);
+    }
+
+    public static GVUser getUser() {
+        return user;
+    }
+}
