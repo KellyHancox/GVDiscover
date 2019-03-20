4269bf080943ccd3b91af37bb6661c50f71ec577
matthewshan <matthewshan99@gmail.com>
2019-03-19 22:04:18 -0400

    Created getEventsList() method.

--- app/src/main/java/com/example/gvdiscoverapp/CreateEvents.java
+++ app/src/main/java/com/example/gvdiscoverapp/CreateEvents.java
@@ -1,11 +1,13 @@
 package com.example.gvdiscoverapp;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.sql.Wrapper;
 
@@ -13,7 +15,7 @@ public class CreateEvents extends AppCompatActivity {
     /** Object that represents eventName input */
     private EditText eventName;
     /** Object that represents location wrapper */
-    private Wrapper location;
+    private Spinner location;
     /** Object that represents startDate input */
     private EditText startDate;
     /** Object that represents startTime input */
@@ -37,7 +39,7 @@ public class CreateEvents extends AppCompatActivity {
         Model model = Model.getInstance();
 
         eventName = (EditText)findViewById(R.id.eventName);
-        location = (Wrapper)findViewById(R.id.location);
+        //location = (Spinner)findViewById(R.id.location);
         startDate = (EditText)findViewById(R.id.startDate);
         startTime = (EditText)findViewById(R.id.startTime);
         endTime = (EditText)findViewById(R.id.endTime);
@@ -47,12 +49,15 @@ public class CreateEvents extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String name = eventName.getText().toString();
-                String event = eventName.getText().toString();
+                String event = "Kirkof" + "~~"
+                        + startDate.getText().toString() + "~~"
+                        + startTime.getText().toString() + "~~"
+                        + endTime.getText().toString() + "~~"
+                        + description.getText().toString() ;
                 Model.addEvent(name, event);
+                System.out.println("\nEvent Name: " + name);
+                System.out.println("Event: " + event);
             }
         });
-
-
     }
-
 }
--- app/src/main/java/com/example/gvdiscoverapp/Model.java
+++ app/src/main/java/com/example/gvdiscoverapp/Model.java
@@ -1,6 +1,8 @@
 package com.example.gvdiscoverapp;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Model {
     /** An instance of the Singleton model */
@@ -9,11 +11,11 @@ public class Model {
     private static GVUser user;
     /** Holds all the events in the database.
      * Value format: "location~~date~~startTime~~endTime~~desc */
-    public static HashMap<String, String> events;
+    public static Map<String, String> events;
 
 
     private Model() {
-        events = new HashMap<>();
+        events = new HashMap<String, String>();
     }
 
     public static Model getInstance() {
@@ -43,8 +45,17 @@ public class Model {
      *
      *  @return Hash map were the key is the name of the event.
      * */
-    public static HashMap<String, String> getEvents() {
+    public static Map<String, String> getEvents() {
         return events;
+    }
+
+    public static ArrayList<String> getEventsList() {
+        ArrayList<String> list = new ArrayList<String>();
+        for (Map.Entry<String,String> entry : events.entrySet()) {
+            list.add(entry.getKey() + "~~" + entry.getValue());
+            System.out.println(entry.getKey() + "~~" + entry.getValue());
+        }
+        return list;
     }
 
     public static void SignUp(String eventName) {
